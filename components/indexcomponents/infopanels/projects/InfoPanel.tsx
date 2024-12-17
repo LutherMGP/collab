3bda63679bdd269faf4756c2abf370a548d9868e
LutherMGP <mgrosenp@outlook.dk>
2024-12-17 09:54:08 +0100

    opdatering

--- components/indexcomponents/infopanels/projects/InfoPanel.tsx
+++ components/indexcomponents/infopanels/projects/InfoPanel.tsx
@@ -1004,7 +1004,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: "white",
   },
   modalContent: {
     width: "90%",
